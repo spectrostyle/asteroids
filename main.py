96e028f74c8dc4dc5b7cb3e422b9e28ddcbeddfd
spectrostyle <spectrostyle@gmail.com>
2025-05-07 01:15:07 -0400

    added game clock

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ with contextlib.redirect_stdout(io.StringIO()):
 
 
 def main():
-	pygame.init
+	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(0)
 		pygame.display.flip()
+		time = clock.tick(60)
+		dt = (time / 1000)
 
 
 if __name__ == "__main__":
