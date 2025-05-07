d68677bb702464c824591608c56760817d9003fe
spectrostyle <spectrostyle@gmail.com>
2025-05-07 03:43:53 -0400

    added class support to draw objects

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from constants import *
 import contextlib
 import io
+import player
 
 with contextlib.redirect_stdout(io.StringIO()):
 	import pygame
@@ -11,12 +12,14 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+	player1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill(0)
+		player1.draw(screen)
+
 		pygame.display.flip()
 		time = clock.tick(60)
 		dt = (time / 1000)
