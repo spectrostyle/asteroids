c844f53badd0dea354a8add579308791fab02af1
spectrostyle <spectrostyle@gmail.com>
2025-05-10 13:00:08 -0400

    cleaned code + viewed answer

--- asteroid.py
+++ asteroid.py
@@ -5,9 +5,6 @@ from circleshape import CircleShape
 class Asteroid(CircleShape):
 	def __init__(self, x, y, radius):
 		super().__init__(x, y, radius)
-		self.x = x
-		self.y = y
-		self.radius = radius
 
 	def draw(self, screen):
 		pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
--- main.py
+++ main.py
@@ -9,28 +9,33 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
-	dt = 0
 
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
 
-	Player.containers = (updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers = (updatable)
+	asteroids = AsteroidField()
+
+	Player.containers = (updatable, drawable)
 
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-	asteroids = AsteroidField()
+
+	dt = 0
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
-		screen.fill("black")
 		updatable.update(dt)
-		for drawables in drawable:
-			drawables.draw(screen)
+
+		screen.fill("black")
+
+		for obj in drawable:
+			obj.draw(screen)
+
 		pygame.display.flip()
 
 		# to limit the framerate to 60 FPS
