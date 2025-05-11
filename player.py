f5f0b68c7f0cb6859765100742ed371241f751a3
spectrostyle <spectrostyle@gmail.com>
2025-05-11 13:45:36 -0400

    timer

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.timer = 0
 
 	def draw(self, screen):
 		pygame.draw.polygon(screen, "white", self.triangle(), 2)
